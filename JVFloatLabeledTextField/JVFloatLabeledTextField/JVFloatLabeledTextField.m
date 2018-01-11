cc9763618a26c6f7984d48feb73e3dc4cca223e8
Phong Cao <ctphong94@gmail.com>
2018-01-11 11:17:48 +0700

    Update JVFloatLabeledTextField.m

--- JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.m
+++ JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.m
@@ -368,7 +368,7 @@ static CGFloat const kFloatingLabelHideAnimationDuration = 0.3f;
     
     BOOL firstResponder = self.isFirstResponder;
     //_floatingLabel.textColor = (firstResponder && self.text && self.text.length > 0 ?
-                                self.labelActiveColor : self.floatingLabelTextColor);
+    //                            self.labelActiveColor : self.floatingLabelTextColor);
     if ((!self.text || 0 == [self.text length]) && !self.alwaysShowFloatingLabel) {
         [self hideFloatingLabel:firstResponder];
     }
